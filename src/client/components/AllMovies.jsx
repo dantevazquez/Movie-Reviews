5233446b7d5a653e8bbf1c940014f57df802c257
Dante Vazquez <dante@Dantes-Air.lan>
2024-01-04 23:42:57 -0800

    added an admin page and styled movie page and navbar

--- src/client/components/AllMovies.jsx
+++ src/client/components/AllMovies.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+// Import necessary libraries
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
+// AllMovies component
 function AllMovies() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  console.log("movies", movies);
 
   useEffect(() => {
     async function getMovies() {
@@ -28,24 +28,30 @@ function AllMovies() {
 
   return (
     <div>
-      <h2>All Movies</h2>
-
       <input
+        className='search-bar'
         type="text"
         placeholder="Search by movie name"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
 
-      {filteredMovies.map((movie) => (
-        <Link key={movie.id} to={`/movies/${movie.id}`} className="link-style">
-          <div style={{ border: "2px solid black", cursor: 'pointer' }}>
-            <h3>{movie.name}</h3>
-            <img src={movie.imgLink} alt={movie.name} style={{ maxWidth: '100%' }} />
-            <p>Average Rating: {movie.avgRating}</p>
-          </div>
-        </Link>
-      ))}
+      <div className="movies-container">
+        {filteredMovies.map((movie) => (
+          <Link key={movie.id} to={`/movies/${movie.id}`} className="link-style">
+            <div className="movie-item">
+              <img src={movie.imgLink} alt={movie.name} />
+              <h3 className='movie-name'>{movie.name}</h3>
+
+              <div className='rating-container'>
+                <p className='avg-rating'>{movie.avgRating.toFixed(1)}</p>
+                <img src="https://img.icons8.com/doodle/48/potato--v1.png" alt="Potato" className="potato-image" />
+              </div>
+
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
